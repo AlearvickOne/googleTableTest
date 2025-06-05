228aad98486a0dbe4bd699ea08d57b34283e41de
AlearvickOne <Alexandr73.73.73.73@mail.ru>
2025-06-05 12:45:02 +0300

    Initial commit

---
+++ apps/frontend/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@google-table/frontend" />
+    </div>
+  );
+}
+
+export default App;
